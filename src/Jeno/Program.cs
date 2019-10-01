787f2abdf41ac1b5943b97420546560c7d4ae77a
jbobek <jakub.bobek.1991@gmail.com>
2019-10-01 19:05:02 +0200

    initial commit

---
+++ src/Jeno/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Jeno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
