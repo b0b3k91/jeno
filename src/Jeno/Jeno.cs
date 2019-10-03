cc3c8162358875b44c664c68d1a5966f69a71849
jbobek <jakub.bobek.1991@gmail.com>
2019-10-03 21:14:32 +0200

    Move GitWrapper to Services directory

---
+++ src/Jeno/Jeno.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Jeno.Services;
+using Jeno.Commands;
+
+namespace Jeno
+{
+    class Jeno
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            var git = new GitWrapper();
+            var branch = git.GetCurrentBranch(System.IO.Directory.GetCurrentDirectory());
+            Console.WriteLine(branch);
+        }
+    }
+}
--- src/Jeno/Services/GitWrapper.cs
+++ src/Jeno/Services/GitWrapper.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 
-namespace Jeno
+namespace Jeno.Services
 {
     class GitWrapper
     {
