3573c29442d89455b8033172cadcde23b9d1f73d
jbobek <jakub.bobek.1991@gmail.com>
2020-04-05 10:45:10 +0200

    Add jeno location to path permanently

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -6,7 +6,6 @@ param(
 $ErrorActionPreference = "Stop"
     
 $root = Resolve-Path (Join-Path $PSScriptRoot "..\")
-
 Push-Location $root
 
 $oldPath = $env:Path -split ';' | Where-Object {$_ -like '*jeno*' }
@@ -17,14 +16,15 @@ if($oldPath){
     if(Test-Path $oldPath){
         Remove-Item $oldPath -Force -Recurse
     }
-    
-    $env:Path = ($env:Path -split ";" | Where-Object {$_ -ne $oldPath }) -join ";"
+
+    $newPath = ($Env:Path -split ";" | Where-Object {$_ -ne $oldPath }) -join ";"
+    [Environment]::SetEnvironmentVariable("Path", $newPath, [System.EnvironmentVariableTarget]::Machine)
 }
 
-Write-Output "Publish Jeno file in selected location"
+Write-Output "Publish Jeno in selected location"
 dotnet publish .\src\Jeno\Jeno.csproj --configuration Release --output $InstallPath /p:DebugType=None
 
-Write-Output "Add Jeno location to environment variables"
-$env:Path += ";$InstallPath"
+Write-Output "Add Jeno to environment variables"
+[Environment]::SetEnvironmentVariable("Path", ($Env:Path += ";$InstallPath"), [System.EnvironmentVariableTarget]::Machine)
 
 Pop-Location
