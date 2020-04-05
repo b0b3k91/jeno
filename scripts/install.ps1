a391f6810ceaa73a260fddff29df439d8995cd61
jbobek <jakub.bobek.1991@gmail.com>
2020-04-05 09:32:11 +0200

    Add installation script

---
+++ scripts/install.ps1
@@ -0,0 +1,30 @@
+#Requires -RunAsAdministrator
+
+param(
+    [string]$InstallPath = "C:\Program Files\jeno"
+)
+$ErrorActionPreference = "Stop"
+    
+$root = Resolve-Path (Join-Path $PSScriptRoot "..\")
+
+Push-Location $root
+
+$oldPath = $env:Path -split ';' | Where-Object {$_ -like '*jeno*' }
+
+if($oldPath){
+    
+    Write-Host "Cleaning previous installation"
+    if(Test-Path $oldPath){
+        Remove-Item $oldPath -Force -Recurse
+    }
+    
+    $env:Path = ($env:Path -split ";" | Where-Object {$_ -ne $oldPath }) -join ";"
+}
+
+Write-Output "Publish Jeno file in selected location"
+dotnet publish --configuration Release --output $InstallPath
+
+Write-Output "Add Jeno location to environment variables"
+$env:Path += ";$InstallPath"
+
+Pop-Location
