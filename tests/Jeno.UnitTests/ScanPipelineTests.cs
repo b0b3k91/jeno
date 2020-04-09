a395fc1a5a11efc023623446c1daace79e03bf9a
jbobek <jakub.bobek.1991@gmail.com>
2020-04-09 17:28:16 +0200

    Prepare basic test set

---
+++ tests/Jeno.UnitTests/ScanPipelineTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Jeno.UnitTests
+{
+    [TestFixture]
+    class ScanPipelineTests
+    {
+        [Test]
+        public void RunScanOutsideRepository_InformAboutIt()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void CannotFindMatchedRepo_InformAboutIt()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void RunScanFromMappedRepository_CreateProperUrl()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void ScanWasSuccessful_ReturnOkCode()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void SomeErrorOccuredInScanRequest_DisplayErrorMessage()
+        {
+            Assert.Fail();
+        }
+    }
+}
